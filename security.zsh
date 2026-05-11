e0257fab96448a2b5e4651bfe04f10e528ce3b32
Dayanna007 <dagaguancelaca@uide.edu.ec>
2026-05-11 15:08:57 +0000

    two

---
+++ security.zsh
@@ -0,0 +1,14 @@
+#
+mkdir -p ~/lab_chgrp/{proyectos,reportes,scripts}
+#
+touch ~/lab_chgrp/proyectos/app.py
+#
+touch ~/lab_chgrp/proyectos/config.json
+#
+touch ~/lab_chgrp/reportes/informe.txt
+#
+touch ~/lab_chgrp/scripts/deploy.sh
+#
+ls -la ~/lab_chgrp/proyectos/
+#
+ls -la ~/lab_chgrp/reportes/ 
