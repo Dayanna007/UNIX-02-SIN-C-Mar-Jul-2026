f72784a57af2e7f73b819e14042c1337b80aa4d8
Dayanna007 <dagaguancelaca@uide.edu.ec>
2026-05-27 23:11:02 +0000

    comandos script

---
+++ script_gnupg.sh
@@ -0,0 +1,41 @@
+#
+ls
+#
+ls Documents
+#
+aptitude moo
+#
+ls -l
+#
+ls -r
+#
+ls -l -r
+#
+ls -lr
+#
+aptitude moo
+#
+aptitude -v moo
+#
+aptitude -vv moo
+#
+aptitude -vvv moo
+#
+aptitude -v -v moo
+#
+pwd 
+#
+cd Documents
+#
+cd /
+#
+cd /home/codespace con cd /home/sysadmin no me funciono
+#
+pwd
+#
+cd Documents
+#
+cd School/Art
+#
+pwd
+
