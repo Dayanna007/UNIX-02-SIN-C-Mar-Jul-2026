45a79d15dc1bde56bb054f35648e40ab2ba25474
Dayanna007 <dagaguancelaca@uide.edu.ec>
2026-05-28 01:11:02 +0000

    Second

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -38,4 +38,54 @@ cd Documents
 cd School/Art
 #
 pwd
+# 
+cd ..
+#
+cd ~
+#
+ls
+#
+ls -l /var/log/
+#
+ls -lSr /var/log
+#
+ls -r /var/log
+#
+su - o sudo su
+#
+exit
+#
+sl 
+#
+su - o sudo su
+#
+sudo apt update
+sudo apt install sl -y
+#
+/usr/games/sl o sl
+#
+exit
+#
+ls
+#
+ls -l
+#
+ls -l hello.sh
+#
+nano hello.sh
+    DENTRO:
+    #!/bin/bash
+    echo "Hola mundo"
+#
+ls -l hello.sh
+#
+chmod +x hello.sh
+#
+ls -l hello.sh
+#
+./hello.sh
+#
+chmod u+x hello.sh
+#
+ls -l hello.sh
 
