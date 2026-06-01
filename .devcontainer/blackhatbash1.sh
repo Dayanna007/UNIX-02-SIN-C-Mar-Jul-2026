0e31260c0f758f70b4a9bb0f8556698f54fe9d8c
Dayanna007 <dagaguancelaca@uide.edu.ec>
2026-06-01 15:19:59 +0000

    mm

--- .devcontainer/blackhatbash1.sh
+++ .devcontainer/blackhatbash1.sh
@@ -1,6 +1,22 @@
+#!/bin/bash 
+set  -x
+
 bash --version 
 env
 echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
+ps -e -f
+ps -ef
+df --human-readable
+#!/bin/bash
+#!/usr/bin/env bash
+#!/bin/bash -x
+/bin/bash -r restricted
+bash -balckhatbash1.sh
+#!/bin/bash 
+echo "Hello World!"
+
+set +x
+bash -balckhatbash1.sh
