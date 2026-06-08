fe75f5d4ee7bcf2a7c10be5f583463de81e90339
Dayanna007 <dagaguancelaca@uide.edu.ec>
2026-06-08 15:21:40 +0000

    ping

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -4,3 +4,6 @@ TARGET="${1}"
 echo "Running the script ${SCRIPT_NAME}..." 
 echo "Pinging the target: ${TARGET}..." 
 ping -c3 "${TARGET}"
+#bash ping_with_arguments.sh 127.0.0.1
+echo "The arguments are: $@"
+echo "The total number of arguments is: $#"
