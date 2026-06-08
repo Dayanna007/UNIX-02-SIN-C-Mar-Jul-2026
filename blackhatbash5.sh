350a80e683b4216a934eb0cfbf615ccb6eb06458
Dayanna007 <dagaguancelaca@uide.edu.ec>
2026-06-08 15:51:11 +0000

    5

---
+++ blackhatbash5.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+touch test && touch test123
+(ls; ps)
+ls; ps; whoami
+lzl || echo "el lzl commando fallo tio"
+echo "Hello World!" > output.txt
+cat output.txt
+echo "bye  World!" > output.txt
+cat output.txt
+echo "bye  World!" >> output.txt
+cat output.txt
+ls -l / &> stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stderr.txt
+ls -l / 1> stdout.txt 2>> stderr.txt
+cat stderr.txt
+lzl 2> error.txt
+cat error.txt
+cat < output.txt
+cat << EOF Black Hat Bash by No Starch Press EOF
