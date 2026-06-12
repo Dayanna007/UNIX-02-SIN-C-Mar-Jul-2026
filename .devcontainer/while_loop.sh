bac12fdb026fa5520378df39c60f5ab31a3a20ab
Dayanna007 <dagaguancelaca@uide.edu.ec>
2026-06-12 15:36:25 +0000

    LISTO

--- .devcontainer/while_loop.sh
+++ .devcontainer/while_loop.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 # File name used to stop the loop
+# Creates a variable that stores the name of the file used to stop the loop.
 SIGNAL_TO_STOP_FILE="stoploop"
 
+# Continues looping while the file does NOT exist.
+# The -f test checks whether the file exists.
+# The ! operator negates the test result.
 # Loop while the file does not exist
 while [[ ! -f "${SIGNAL_TO_STOP_FILE}" ]]; do
+
+    # Displays a message indicating that the file has not been found.
     echo "The file ${SIGNAL_TO_STOP_FILE} does not yet exist..."
+
+    # Informs the user that the script will check again after 2 seconds.
     echo "Checking again in 2 seconds..."
+
+    # Pauses the script for 2 seconds before the next iteration.
     sleep 2
+# Ends the while loop.
 done
 
-# Exit when the file is found
+# Displays a message when the file is detected and the script exits.
 echo "File was found! Exiting..."
