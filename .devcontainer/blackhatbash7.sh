6c0c1ef90481fa0c3a168d22148521add50b8b3d
Dayanna007 <dagaguancelaca@uide.edu.ec>
2026-06-19 15:20:48 +0000

    grep

--- .devcontainer/blackhatbash7.sh
+++ .devcontainer/blackhatbash7.sh
@@ -6,3 +6,10 @@ done
 for ip_address in "$@"; do
   echo "Taking some action on IP address ${ip_address}"
 done
+
+
+grep "35.237.4.214\|13.66.139.0" log.txt
+grep -e "35.237.4.214" -e "13.66.139.0" log.txt
+ps | grep TTY
+ps | grep tty
+ps | grep -i tty
